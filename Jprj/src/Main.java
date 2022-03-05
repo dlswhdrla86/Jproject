40d525eba6a521f9fe88d6468433b411e2fde433
JONGIN <JONGIN@SJ-PC>
2022-03-05 19:29:20 +0900

    1푸쉬 실패 2번쨰 푸쉬

--- Jprj/src/Main.java
+++ Jprj/src/Main.java
@@ -4,6 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("2¹ø¤Š Ä¿¹Ô");
 		System.out.println("3¹ø¤Š Ä¿¹Ô");
+		System.out.println("4¹ø¤Š Ä¿¹Ô");
 	}
 	
 }
