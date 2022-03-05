17707f2caefd202d1b430c8849d8400d5126931c
JONGIN <JONGIN@SJ-PC>
2022-03-05 19:46:15 +0900

    gogogogo

--- Jprj/src/Main.java
+++ Jprj/src/Main.java
@@ -2,9 +2,9 @@
 public class Main {
 	
 	public static void main(String[] args) {
-		System.out.println("2¹ø¤Š Ä¿¹Ô");
-		System.out.println("3¹ø¤Š Ä¿¹Ô");
-		System.out.println("4¹ø¤Š Ä¿¹Ô");//
+		System.out.println("111");
+		System.out.println("222");
+		System.out.println("333");
 	}
 	
 }
