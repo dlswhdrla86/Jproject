66ea1c1bfe70b4fdb710f11819ff260f7ea4d4fa
dlswhdrla86 <100995062+dlswhdrla86@users.noreply.github.com>
2022-03-23 00:57:12 +0900

    Update B.java
    tt

--- Jprj/src/B.java
+++ Jprj/src/B.java
@@ -2,7 +2,8 @@
 public class B {
 	public static void main(String[] args) {
 		System.out.println("1 commit");
-		System.out.println("git에서 수정함");
+		System.out.println("gittest1");
+		System.out.println("gittest2");
 		System.out.println("1 push");
 	}
 }
